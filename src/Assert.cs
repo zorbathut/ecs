79626f4b981f2f45149ad5d67d9c0722a06c2180
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-05-24 05:29:44 -0500

    Fix: Errors when a system with a singleton parameter also touches multiple tranches.

--- src/Assert.cs
+++ src/Assert.cs
@@ -1,5 +1,7 @@
 namespace Ghi;
 
+using System;
+
 internal static class Assert
 {
     public static void IsNull(object val)
@@ -44,6 +46,39 @@ internal static class Assert
 
     public static void AreEqual<T>(T expected, T actual)
     {
+        // both of these could inherit from T, so let's just verify no inheritance games are going on here
+        // at least if they end up different
+        if (expected.GetType() != actual.GetType())
+        {
+            Dbg.Err($"Types do not match: expected {expected.GetType()}, actual {actual.GetType()}");
+            return;
+        }
+
+        // arrays should really have Equals overridden
+        if (expected is Array)
+        {
+            // can't use SequenceEqual because we don't have typing info
+            var expectedArray = (Array)(object)expected;
+            var actualArray = (Array)(object)actual;
+
+            if (expectedArray.Length != actualArray.Length)
+            {
+                Dbg.Err($"Array lengths do not match: expected {expectedArray.Length}, actual {actualArray.Length}");
+                return;
+            }
+
+            for (int i = 0; i < expectedArray.Length; i++)
+            {
+                if (!expectedArray.GetValue(i).Equals(actualArray.GetValue(i)))
+                {
+                    Dbg.Err($"Values do not match at index {i}: expected {expectedArray.GetValue(i)}, actual {actualArray.GetValue(i)}");
+                    return;
+                }
+            }
+
+            return;
+        }
+
         if (!expected.Equals(actual))
         {
             Dbg.Err($"Values do not match: expected {expected}, actual {actual}");
